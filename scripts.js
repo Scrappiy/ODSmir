da20780dc45dbe8af3eef2976b7718be59db22db
Scrappiy <127025665+Scrappiy@users.noreply.github.com>
2023-06-25 21:51:04 +0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -319,7 +319,7 @@ function obr6() {
 
 function obr7() {
     let a = "", b = "", c = "", d = "", res1 = "", res2 = "", Py = 0, Px = 0;
-   a = document.getElementById("Y7").value;
+    a = document.getElementById("Y7").value;
     b = document.getElementById("X8").value;
     if ((a == "") || (b == "")) {
         alert(`ERROR!`);
@@ -329,10 +329,10 @@ function obr7() {
         let Xmas = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
         let Ymas = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-        if ((a.length % 3) == 1) { a = a + " "; }
-        if ((a.length % 3) == 2) { a = a + "  "; }
-        if ((b.length % 3) == 1) { b = b + " "; }
-        if ((b.length % 3) == 2) { b = b + "  "; }
+        if ((a.length % 3) == 1) { a = a + "  "; }
+        if ((a.length % 3) == 2) { a = a + " "; }
+        if ((b.length % 3) == 1) { b = b + "  "; }
+        if ((b.length % 3) == 2) { b = b + " "; }
 
         for (let index = 0; index < (a.length / 3); index++) {
             res1 = a[3 * index] + a[3 * index + 1] + a[3 * index + 2];
@@ -368,53 +368,141 @@ function obr7() {
             }
 
         }
-for (let index = 0; index < (a.length / 3); index++) {
-    Xmas[index]=index+1;
-    Ymas[index]=index+1;
-}
-
-for (let index = 0; index < (a.length / 3); index++) {
-
-    for (let index1 = 0; index1 < (a.length / 3); index1++) {
-        if (NYmas[index] < NYmas[index1]) {
-            Px = Ymas[index];
-            Ymas[index] = Ymas[index1];
-            Ymas[index1] = Px;
-            Px = NYmas[index];
-            NYmas[index] = NYmas[index1];
-            NYmas[index1] = Px;
-        }
-
-        if (NXmas[index] < NXmas[index1]) {
-            Px = Xmas[index];
-            Xmas[index] = Xmas[index1];
-            Xmas[index1] = Px;
-            Px = NXmas[index];
-            NXmas[index] = NXmas[index1];
-            NXmas[index1] = Px;
-        }
-    }
-
-}
-
-c="Rx : ";
-d="Ry : ";
-
-        document.getElementById("vvd7").innerHTML= Xmas+`<br>` + Ymas;
-Py=0;
         for (let index = 0; index < (a.length / 3); index++) {
-            Px=Xmas[index]-Ymas[index];
-            res1=Xmas[index];
-            c=c+res1+" ";
-            res1=Ymas[index];
-            d=d+res1+" ";
-            Px=Px*Px;
-            Py=Py+Px;
+            Xmas[index] = index + 1;
+            Ymas[index] = index + 1;
+        }
+
+        for (let index = 0; index < (a.length / 3); index++) {
+
+            for (let index1 = 0; index1 < (a.length / 3); index1++) {
+                if (NYmas[index] < NYmas[index1]) {
+                    Px = Ymas[index];
+                    Ymas[index] = Ymas[index1];
+                    Ymas[index1] = Px;
+                    Px = NYmas[index];
+                    NYmas[index] = NYmas[index1];
+                    NYmas[index1] = Px;
+                }
+
+                if (NXmas[index] < NXmas[index1]) {
+                    Px = Xmas[index];
+                    Xmas[index] = Xmas[index1];
+                    Xmas[index1] = Px;
+                    Px = NXmas[index];
+                    NXmas[index] = NXmas[index1];
+                    NXmas[index1] = Px;
+                }
+            }
+
+        }
+
+        c = "Rx : ";
+        d = "Ry : ";
+
+        document.getElementById("vvd7").innerHTML = Xmas + `<br>` + Ymas;
+        Py = 0;
+        for (let index = 0; index < (a.length / 3); index++) {
+            Px = Xmas[index] - Ymas[index];
+            res1 = Xmas[index];
+            c = c + res1 + " ";
+            res1 = Ymas[index];
+            d = d + res1 + " ";
+            Px = Px * Px;
+            Py = Py + Px;
         }
 
 
-Px=1-(6*(Py/(((a.length / 3)*(a.length / 3)*(a.length / 3))-(a.length / 3))));
-res1 =c+`<br>`+d+`<br>`+`<br>`+"Сумма квадратов отк = "+ Py +", коэффициент корреляции рангов = "+ Px;
-        document.getElementById("vvd7").innerHTML= res1;
+        Px = 1 - (6 * (Py / (((a.length / 3) * (a.length / 3) * (a.length / 3)) - (a.length / 3))));
+        res1 = c + `<br>` + d + `<br>` + `<br>` + "Сумма квадратов отк = " + Py + ", коэффициент корреляции рангов = " + Px;
+        document.getElementById("vvd7").innerHTML = res1;
+    }
+}
+
+function obr8() {
+    let a = "", b = "", res1 = "", res2 = "", Py = 0, Px = 0, x = 0;
+    b = document.getElementById("Y8").value;
+    a = document.getElementById("X9").value;
+    if ((a == "") || (b == "")) {
+        alert(`ERROR!`);
+    } else {
+        let NXmas = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        let NYmas = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        let Xmas = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        let Ymas = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+        if ((a.length % 3) == 1) { a = a + "  "; }
+        if ((a.length % 3) == 2) { a = a + " "; }
+        if ((b.length % 3) == 1) { b = b + "  "; }
+        if ((b.length % 3) == 2) { b = b + " "; }
+
+        for (let index = 0; index < (a.length / 3); index++) {
+            res1 = a[3 * index] + a[3 * index + 1] + a[3 * index + 2];
+            Xmas[index] = parseFloat(res1);
+        }
+        res1 = "";
+        for (let index = 0; index < (a.length / 3); index++) {
+            res2 = b[3 * index] + b[3 * index + 1] + b[3 * index + 2];
+            Ymas[index] = parseFloat(res2);
+        }
+        res2 = "";
+
+        for (let index = 0; index < (b.length / 3); index++) {
+
+            for (let index1 = 0; index1 < (b.length / 3); index1++) {
+
+                if (Ymas[index] < Ymas[index1]) {
+                    Px = Ymas[index];
+                    Ymas[index] = Ymas[index1];
+                    Ymas[index1] = Px;
+                    Px = Xmas[index];
+                    Xmas[index] = Xmas[index1];
+                    Xmas[index1] = Px;
+                }
+            }
+
+        }
+
+        for (let index = 0; index < (b.length / 3); index++) {
+
+            for (let index1 = 0; index1 < (b.length / 3); index1++) {
+                if (Xmas[index] < Xmas[index1]) {
+                    Px = Xmas[index];
+                    Xmas[index] = Xmas[index1];
+                    Xmas[index1] = Px;
+                    Px = Ymas[index];
+                    Ymas[index] = Ymas[index1];
+                    Ymas[index1] = Px;
+                    Px = NYmas[index];
+                    NYmas[index] = NYmas[index1];
+                    NYmas[index1] = Px;
+                }
+            }
+
+        }
+
+        Px = 0;
+        for (let index = 0; index < (b.length / 3); index++) {
+
+            for (let index1 = index; index1 < (b.length / 3); index1++) {
+                if (Ymas[index] < Ymas[index1]) {
+                    Px = Px + 1;
+                }
+            }
+
+        }
+        Py = 0;
+        for (let index = 0; index < (b.length / 3); index++) {
+
+            if (NYmas[index] > NXmas[index]) {
+                Py = Py + (NYmas[index] - NXmas[index])
+            }
+
+        }
+
+        x = (a.length / 3);
+        x = ((Px - Py) / (x * x - x))
+
+        document.getElementById("vvd8").innerHTML = "Массив X : " + Xmas + `<br> Массив Y : ` + Ymas + `<br> P : ` + Px + `<br> Q : ` + Py + `<br> t : ` + 2 * x;
     }
 }
